5a51f81e9f9663479c21367bc08e5698327c645e
orzechcather181 <orzechcather181@gmail.com>
2025-03-13 11:33:28 +0000

    Add 7.go

---
+++ src/7.go
@@ -0,0 +1,13 @@
+// Package main provides ...
+package main
+
+import "fmt"
+
+// Hello returns a greeting message
+func Hello() string {
+	return "Hello, World!"
+}
+
+func main() {
+	fmt.Println(Hello())
+}
